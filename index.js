c98cff95e2720dc9943ab89805e546b4a0ee8edd
fengbin_chen <1563540102@qq.com>
2026-01-18 14:47:39 +0800

    change if(DEBUG) to if('DEBUG')

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
-module.exports = () => {
+//核心思路：转换成AST，然后修改AST树（增删改...），最后生成新的代码
+
+module.exports = ({ type: t }) => {
   return {
-    visitor: {},
+    visitor: {
+      //当遇到Identifier时，就会执行这个函数
+      Indentifier(path) {
+        const parentIsIf = t.isIfStatement(path.parentPath);
+        const isDebug = path.node.name === "DEBUG";
+        if (parentIsIf && isDebug) {
+          //把Indentifier转换成string
+          const stringNode = t.stringLiteral("DEBUG");
+          //替换
+          path.replaceWith(stringNode);
+        }
+      },
+    },
   };
 };
